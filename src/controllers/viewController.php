a19ece0f663768e3771981baeaa3ccb209cf2292
hjl4347570 <hjl4347570@gmail.com>
2017-06-25 23:14:37 +0800

    add

--- src/controllers/viewController.php
+++ src/controllers/viewController.php
@@ -5,7 +5,6 @@ $sites = ['front', 'system', 'chat', 'share', 'webgradient'];
  * multiple sites
  */
 $app->get('/{site:(?:' . join('|', $sites) . ')}{path:.*}', function ($req, $res, $args) {
-    setcookie('site', $args['site'], time() + 315360000, '/');
     return $this->renderer->render($res, '/sites/' . $args['site'] . '.html', $args);
 });
 
@@ -16,10 +15,8 @@ $app->get('/', function ($req, $res, $args) {
     $args['env'] = $this->get('settings')['env'];
     $site = $req->getCookieParam('site');
     if ($site) {
-//        return $res->withRedirect('/' . $site);
         return $this->renderer->render($res, '/sites/' . $site . '.html', $args);
     } else {
-        // return $this->renderer->render($res, '/home/index.html', $args);
         return $res->withRedirect('/webgradient');
     }
 });
